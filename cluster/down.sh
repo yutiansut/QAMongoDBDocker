88228f487b136728a515bc761fa1e4adf92ca0a5
barretthugh <barrettyip@gmail.com>
2019-08-04 08:24:35 +0800

    update

--- cluster/down.sh
+++ cluster/down.sh
@@ -1,4 +1,4 @@
-    
+
 #!/bin/bash
 
-docker-compose -f docker-compose.1.yml -f docker-compose.2.yml -f docker-compose.cnf.yml -f docker-compose.shard.yml down --volumes --remove-orphans
+docker-compose down --volumes --remove-orphans
